5fc5515d3b884b03fc850c63ed66ab440eef7169
Stephen Kelzer <stephen@stephenkelzer.com>
2023-09-29 00:13:39 -0500

    adding a lambda?

--- lib/my-stack.ts
+++ lib/my-stack.ts
@@ -1,10 +1,25 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps, CfnParameter } from "aws-cdk-lib";
+import { Function, Code, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
 export default class MyStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    console.log("todo")
+    new Function(this, 'MyLambda', {
+      code: Code.fromCfnParameters({
+        bucketNameParam: new CfnParameter(this, 'MyLambdaBucket', {
+          type: 'String',
+          description: 'The name of the S3 bucket that contains the Lambda code',
+        }),
+        objectKeyParam: new CfnParameter(this, 'MyLambdaKey', {
+          type: 'String',
+          description: 'The name of the S3 key that contains the Lambda code',
+        }),
+      }),
+      handler: 'index.handler',
+      runtime: Runtime.NODEJS_LATEST
+    });
+
   }
 }
