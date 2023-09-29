653ab627bdcea66f0c6885c25f39ed62eae37906
Stephen Kelzer <stephen@stephenkelzer.com>
2023-09-29 16:03:18 -0500

    dynamoDb and list?

--- lib/widgets-stack.ts
+++ lib/widgets-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as cdkLambda from 'aws-cdk-lib/aws-lambda';
+import * as cdkDynamoDb from 'aws-cdk-lib/aws-dynamodb';
 import * as cdkApiGateway from '@aws-cdk/aws-apigatewayv2-alpha';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { Cors } from 'aws-cdk-lib/aws-apigateway';
@@ -14,6 +15,30 @@ export class WidgetsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: WidgetsStackProps) {
     super(scope, id, props);
 
+    const db = new cdkDynamoDb.Table(this, 'WidgetsDb', {
+      tableName: 'widgets',
+      partitionKey: {
+        name: "id",
+        type: cdkDynamoDb.AttributeType.STRING
+      },
+      sortKey: {
+        name: "created",
+        type: cdkDynamoDb.AttributeType.NUMBER
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,// should this be retain?
+      billingMode: cdkDynamoDb.BillingMode.PAY_PER_REQUEST,
+      // deletionProtection: true
+    });
+
+    db.addLocalSecondaryIndex({
+      indexName: 'nameIndex',
+      sortKey: {
+        name: 'name',
+        type: cdkDynamoDb.AttributeType.STRING,
+      },
+      projectionType: cdkDynamoDb.ProjectionType.ALL,
+    });
+
     const apiGateway = new cdkApiGateway.HttpApi(this, `${props.environment}-ApiGateway`, {
       corsPreflight: {
         allowOrigins: Cors.ALL_ORIGINS,
@@ -25,8 +50,13 @@ export class WidgetsStack extends cdk.Stack {
     const listWidgetsLambda = new cdkLambda.Function(this, 'ListWidgetsLambda', {
       code: cdkLambda.Code.fromAsset('./dist'),
       handler: 'list-widgets-lambda.handler',
-      runtime: cdkLambda.Runtime.NODEJS_LATEST
+      runtime: cdkLambda.Runtime.NODEJS_LATEST,
+      environment: {
+        DYNAMO_TABLE_NAME: db.tableName,
+      }
     });
+
+    db.grantReadData(listWidgetsLambda);
 
     apiGateway.addRoutes({
       path: '/widgets',
@@ -37,8 +67,13 @@ export class WidgetsStack extends cdk.Stack {
     const createWidgetLambda = new cdkLambda.Function(this, 'CreateWidgetLambda', {
       code: cdkLambda.Code.fromAsset('./dist'),
       handler: 'create-widget-lambda.handler',
-      runtime: cdkLambda.Runtime.NODEJS_LATEST
+      runtime: cdkLambda.Runtime.NODEJS_LATEST,
+      environment: {
+        DYNAMO_TABLE_NAME: db.tableName,
+      }
     });
+
+    db.grantWriteData(createWidgetLambda);
 
     apiGateway.addRoutes({
       path: '/widgets',
--- src/list-widgets-lambda.ts
+++ src/list-widgets-lambda.ts
@@ -1,14 +1,39 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResult } from 'aws-lambda'
+import { DynamoDB, ScanInput } from '@aws-sdk/client-dynamodb'
 
 export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResult> => {
-    console.log('GET Widgets', { event })
+    console.log('LIST Widgets', { event })
 
-    if (event.queryStringParameters?.error) {
-        throw new Error('This is an error!')
+    const dynamoClient = new DynamoDB({
+        // region: 'us-west-2'
+    })
+
+    const scanTodo: ScanInput = {
+        TableName: process.env.DYNAMO_TABLE_NAME,
+        Limit: 10
     }
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'You made it!' })
+    try {
+
+        const results = await dynamoClient.scan(scanTodo);
+        console.log({ results })
+
+        // const userData = Items ? Items.map(item => unmarshall(item)) : []
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ results })
+        }
+
+    } catch (err) {
+
+        console.log(err)
+
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'something went wrong'
+            })
+        }
     }
 }
