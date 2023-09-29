9bf47a7ab628d5d8ae7c27cc62d4120e9ff88c56
Stephen Kelzer <stephen@stephenkelzer.com>
2023-09-29 13:55:23 -0500

    adding an api gateway?

--- lib/widgets-stack.ts
+++ lib/widgets-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cdkLambda from 'aws-cdk-lib/aws-lambda';
+import * as cdkApiGateway from '@aws-cdk/aws-apigatewayv2-alpha';
+import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 
 export interface WidgetsStackProps extends cdk.StackProps {
   environment: 'test' | 'local' | 'staging' | 'production';
@@ -10,10 +12,33 @@ export class WidgetsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: WidgetsStackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, 'Lambda', {
-      code: lambda.Code.fromAsset('./dist'),
+    const apiGateway = new cdkApiGateway.HttpApi(this, 'ApiGateway', {
+      corsPreflight: {
+        allowHeaders: [
+          'Content-Type',
+        ],
+        allowMethods: [
+          cdkApiGateway.CorsHttpMethod.GET,
+        ],
+        // allowCredentials: true,
+        allowOrigins: ['http://localhost:3000'],
+      },
+    });
+
+    const lambda = new cdkLambda.Function(this, 'Lambda', {
+      code: cdkLambda.Code.fromAsset('./dist'),
       handler: 'get-widgets-lambda.getWidgets',
-      runtime: lambda.Runtime.NODEJS_LATEST
+      runtime: cdkLambda.Runtime.NODEJS_LATEST
+    });
+
+    apiGateway.addRoutes({
+      path: '/widgets',
+      methods: [cdkApiGateway.HttpMethod.GET],
+      integration: new HttpLambdaIntegration('get-todos-integration', lambda),
+    });
+
+    new cdk.CfnOutput(this, 'api_gateway_url', {
+      value: apiGateway.url ?? "unknown",
     });
   }
 }
