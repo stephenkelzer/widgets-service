b66f1a28195b6381f114a81800ee3a2e5cb6d86d
Stephen Kelzer <stephen@stephenkelzer.com>
2023-09-29 22:37:46 -0500

    huh

--- lib/widgets-stack.ts
+++ lib/widgets-stack.ts
@@ -16,7 +16,7 @@ export class WidgetsStack extends cdk.Stack {
     super(scope, id, props);
 
     const db = new cdkDynamoDb.Table(this, 'WidgetsDb', {
-      tableName: 'widgets',
+      tableName: 'widgets1',
       partitionKey: {
         name: "id",
         type: cdkDynamoDb.AttributeType.STRING
